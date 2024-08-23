3e88f13bfdd8596cc45480a093192149e0ba7f83
RoughBoy0723 <ohwy990723@gmail.com>
2024-08-23 16:42:04 +0900

    7-1

--- 7-1/main.cpp
+++ 7-1/main.cpp
@@ -8,8 +8,17 @@ private:
     int gasolineGauge;
 
 public:
+    car():gasolineGauge(0){}
+    car(int n):gasolineGauge(n){
+        cout << "car(int n)\n";
+    }
+
     int GetGasGauge(){
         return gasolineGauge;
+    }
+    
+    ~car(){
+        cout << "Delete car\n";
     }
 };
 
@@ -17,8 +26,17 @@ class HybridCar : public car{
 private:
     int electriGauge;
 public:
-    int ShowCurrentGauge(){
+    HybridCar():electriGauge(0){}
+    HybridCar(int n):electriGauge(n){}
+    HybridCar(int a, int b):car(a),electriGauge(b){
+        cout << "HybridCar(int a, int b)\n";
+    }
+
+    int GetElecGauge(){
         return electriGauge;
+    }
+    ~HybridCar(){
+        cout << "Delete HybridCar\n";
     }
 };
 
@@ -26,13 +44,26 @@ class HybridWaterCar : public HybridCar{
 private:
     int waterGauge;
 public:
+    HybridWaterCar():waterGauge(0){}
+    HybridWaterCar(int n):waterGauge(n){}
+    HybridWaterCar(int a, int b, int c):HybridCar(a, b),waterGauge(c){
+        cout << "HybridWaterCar(int a, int b, int c)\n";
+    }
+
     void ShowCurrentGauge(){
-        cout << "잔여 가솔린" << GetGasGauge() <<endl;
-        cout << "잔여 전기량" << GetElecGauge() <<endl;
-        cout << "잔여 워터량" << waterGauge <<endl;
+        cout << "잔여 가솔린: " << GetGasGauge() <<endl;
+        cout << "잔여 전기량: " << GetElecGauge() <<endl;
+        cout << "잔여 워터량: " << waterGauge <<endl;
+    }
+    ~HybridWaterCar(){
+        cout << "Delete HybridWaterCar\n";
     }
 };
 
 int main(void){
-
+    HybridWaterCar HWC(20,30,40);
+    HWC.ShowCurrentGauge();
 }
+
+
+//nf-ripa
