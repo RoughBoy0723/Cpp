7b99b98b4c0c34c80b6ca55af9e3a220267a4908
RoughBoy0723 <ohwy990723@gmail.com>
2024-08-27 21:15:20 +0900

    10-1 Point

--- 10-1/Point.cpp
+++ 10-1/Point.cpp
@@ -5,27 +5,73 @@ using namespace std;
 
 class Point{
 private:
-    int num;
+    int xpos;
+    int ypos;
 
 public:
-    Point(int _num):num(_num){};
-
-    Point operator-(const Point &ref){
-        Point pos(num - ref.num);
-        return pos;
-    }
-
+    Point(int _xpos, int _ypos):xpos(_xpos), ypos(_ypos){};
+    
     void ShowNum(){
-        cout << num <<endl;
+        cout << '['<< xpos <<','<< ypos <<']' <<endl;
     }
+    
+    friend Point operator-(const Point &ref1, const Point &ref2);
+    friend bool operator==(const Point &ref1, const Point &ref2);
+    friend bool operator!=(const Point &ref1, const Point &ref2);
+    Point operator+=(const Point &ref){
+        this->xpos += ref.xpos;
+        this->ypos += ref.ypos;
+        return *this;
+    };
+    Point operator-=(const Point &ref){
+        this->xpos -= ref.xpos;
+        this->ypos -= ref.ypos;
+        return *this;
+    };
+    
 };
 
+Point operator-(const Point &ref1, const Point &ref2){
+    Point pos(ref1.xpos - ref2.xpos, ref1.ypos - ref2.ypos );
+    return pos;
+}
+
+bool operator==(const Point &ref1,const Point &ref2){
+    if(ref1.xpos == ref2.xpos && ref1.ypos == ref2.ypos){
+        return true;
+    }
+    return false;
+}
+
+bool operator!=(const Point &ref1,const Point &ref2){
+    return !(ref1==ref2);
+}
 
 int main(void){
-    Point pt1(10);
-    Point pt2(3);
+    Point pt1(12, 21);
+    Point pt2(0, 20);
     Point pt3 = pt1 - pt2;
     pt3.ShowNum();
+    Point pt4(1, 12); 
+    (pt4 += pt1).ShowNum();
+    Point pt5(20, 10); 
+    (pt5 -= pt2).ShowNum();
 
+    Point pt6(12,21);
+    if(pt6 == pt1){
+        cout << "1: pt6 == pt1\n";
+    }else{
+        cout << "1: pt6 != pt1\n";
+    }
+
+
+
+    if(pt6 != pt2){
+        cout << "2: pt6 != pt1\n";
+    }else{
+        cout << "2: pt6 == pt1\n";
+    }
+
+    
     return 0;
 }
